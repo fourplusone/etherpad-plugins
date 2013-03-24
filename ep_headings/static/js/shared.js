26c55f9a6fe5fb5bedc81616bbc39e8eff7761b2
John McLear <john@mclear.co.uk>
2013-03-24 04:30:02 +0000

    monospace block support

--- ep_headings/static/js/shared.js
+++ ep_headings/static/js/shared.js
@@ -1,8 +1,9 @@
 var _ = require('ep_etherpad-lite/static/js/underscore');
 
-var tags = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'];
+var tags = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'h7'];
 
 var collectContentPre = function(hook, context){
+  console.log(context);
   var tname = context.tname;
   var state = context.state;
   var lineAttributes = state.lineAttributes
